93a40abb62a392a3bdfa78efa2530cac2aab85c0
jrwhite 🤙 <white8785@gmail.com>
2024-03-23 00:40:45 -0400

    Initial project commit

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Usage: 
+    - curl -X POST -d "new_color=red-500" http://localhost:8000/color
+    - curl -X POST -d "new_color=green-500" http://localhost:8000/color
+"""
+from fastapi import Depends, FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+color = "green"
+
+@app.post("/color")
+async def update_color(new_color: str = Form(...)):
+    global color
+    color = new_color
+    return {"color": color}
+
+@app.get("/", response_class=HTMLResponse)
+async def read_main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "color": color})
